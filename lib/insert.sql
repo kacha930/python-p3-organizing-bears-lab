ae5c9d5533504674bf204d2d477c016bb9c0b9bd
kacha930 <robiwere20@gmail.com>
2024-09-23 12:12:38 +0300

    writing sql query on python file to be mapped on the db

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES('Mr. Chocolate',1,'M','blue','sanguine',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES('Rowdy',2,'F','red','choleric ',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES('Tabitha',3,'M','green','melancholic',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES('Sergeant Brown',5,'F','purple','phlegmatic',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES('Melissa',7,'F','yellow','melancholic',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES('Grinch',10,'M','black','sanguine',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES('Wendy',11,'M','blue','sanguine',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES(NULL,12,'M','blue','sanguine',1);
